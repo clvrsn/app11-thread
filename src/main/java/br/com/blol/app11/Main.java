dcfc0c726a3660f4cae5f2c7724522f2ea653fab
Cleverson <cleverson.carneiro@gmail.com>
2015-10-23 12:44:56 -0200

    ok

--- src/main/java/br/com/blol/app11/Main.java
+++ src/main/java/br/com/blol/app11/Main.java
@@ -4,9 +4,11 @@ package br.com.blol.app11;
  * Created by cleverson on 23/10/15.
  */
 public class Main {
+
     public static void main(String args[]){
 
         MinhaThread t = new MinhaThread();
         t.start();
+
     }
 }
