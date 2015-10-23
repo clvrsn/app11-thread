70a0464d04ba6131b0256f783d2253345b5dff18
Cleverson <cleverson.carneiro@gmail.com>
2015-10-23 12:37:40 -0200

    ok

---
+++ src/main/java/br/com/blol/app11/MinhaThread.java
@@ -0,0 +1,12 @@
+package br.com.blol.app11;
+
+/**
+ * Created by cleverson on 23/10/15.
+ */
+public class MinhaThread extends Thread{
+
+    public void run() {
+        super.run();
+        System.out.println("Olá mundo...");
+    }
+}
